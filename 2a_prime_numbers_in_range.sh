de6214b10924b739a540349147e7c3c9d22d0ffe
satish <satishnda3576@gmail.com>
2024-08-06 09:10:30 +0530

    2a has been modified

--- 2a_prime_numbers_in_range.sh
+++ 2a_prime_numbers_in_range.sh
@@ -1,28 +1,24 @@
 #!/bin/bash
+echo "Enter start : "
+read low;
+echo "Enter end : "
+read high;
 
+echo "the prime between  $low and $high is"
 
-echo "Enter the start: "
-read low
-echo "Enter the end: "
-read high
-
-
-echo "Prime numbers between $low and $high :"
-for ((n=low; n<=high; n++)); do
-    # if [ "$n" -le 1 ]; then
-    #     continue
-    # fi
-   # these above lines are optional ,to handle numbers which are less than 2
-
-
-    is_prime=true
-    for ((i=2; i*i<=n; i++)); do
-        if [ $((n % i)) -eq 0 ]; then
-            is_prime=false
-            break
-        fi
-    done
-    if $is_prime; then
-        echo "$n"
-    fi
+for (( i=low;i<=high;i++ ))
+do
+for (( j=2;j<$i/2;j++ ))
+do
+if [ $((i%j)) == 0 ]
+then
+flag=1
+break
+fi
+done
+if [ $flag == 0 ]
+then
+echo  -ne " $i "
+fi
+flag=0
 done
