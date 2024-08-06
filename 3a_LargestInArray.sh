9c6dc068b5fc0b8dba21f7479b30c371ab3b9987
satish <satishnda3576@gmail.com>
2024-08-06 22:04:44 +0530

    3a additional

--- 3a_LargestInArray.sh
+++ 3a_LargestInArray.sh
@@ -14,3 +14,27 @@ for ((i=1; i<n; i++)); do
     fi
 done
 echo "The largest number is $largest"
+
+
+
+or
+#3a. Finding largest of N numbers (storing numbers in an array)
+#!/bin/bash
+# Program to find the largest of the given array of numbers
+echo "Enter the size of the array: "
+read n
+declare -A arr
+echo "Enter the elements of the array: "
+for ((i=0; i<n; i++))
+do
+read arr["$i"]
+done
+max=${arr[$i]}
+for ((i=1; i<n; i++))
+do
+if [[ $max < ${arr[$i]} ]]
+then
+max=${arr[$i]}
+fi
+done
+echo "The largest of the given numbers is $ma
